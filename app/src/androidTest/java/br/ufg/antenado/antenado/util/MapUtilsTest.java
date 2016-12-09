66371ee94f2bf690606c816ed41a3b88e53e944f
brunovieirandrade <brunoandrade.inf@gmail.com>
2016-12-09 18:01:59 -0200

    Junit tests added

---
+++ app/src/androidTest/java/br/ufg/antenado/antenado/util/MapUtilsTest.java
@@ -0,0 +1,27 @@
+package br.ufg.antenado.antenado.util;
+
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by bruno.andrade on 09/12/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class MapUtilsTest {
+
+    @Test
+    public void getMyLocationTest() {
+        Assert.assertEquals(4, 4);
+    }
+
+    @Test
+    public void testConvertDistanceTest() {
+
+    }
+
+}
